ebd705500036b86bc849c13f01b25a66c0e574c9
Archit Mahule <archit.mahule@mathongo.com>
2025-08-04 21:34:23 +0530

    feat: add VoteForm component and update voting logic to track voters

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { VoteProvider } from './contexts/VoteContext';
 // import Header from './components/Header';
 import CandidateList from './components/CandidateList';
 import VoteSummary from './components/VoteSummary';
+import VoteForm from './components/VoteForm';
 // import Footer from './components/Footer';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <div className="min-h-screen bg-gradient-to-b from-purple-50 to-white flex flex-col">
         {/* <Header title="Class Monitor Election 2025" /> */}
         <main className="flex-grow container mx-auto px-4 py-8">
+          <VoteForm />
           <CandidateList />
           <VoteSummary />
         </main>
